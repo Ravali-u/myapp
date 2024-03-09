d7ad7e1e8c694a2822144cf9a373244755a3b9f7
Ravali-Uppu <ravaliuppu@lyrostech.com>
2024-03-09 15:49:04 +0530

    update changes

--- src/App15.js
+++ src/App15.js
@@ -1,7 +1,20 @@
-import React from 'react'
-
-export const App15 = () => {
+import React from "react";
+import Feeds from "./Feeds";
+import Post from "./Post";
+import Album from "./Album"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+export default function App15() {
   return (
-    <div>App15</div>
-  )
+    <div>
+      <Router>
+        <Link to="/">Feeds</Link> | <Link to="/post">Post</Link>
+        <Routes>
+            <Route path="/" index element={<Feeds/>}></Route>
+            <Route path="/post" element={<Post/>}></Route>
+            <Route path="/album" element={<Album/>}></Route>
+
+        </Routes>
+      </Router>
+    </div>
+  );
 }
