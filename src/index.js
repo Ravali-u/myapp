c195128ec1e0650413df660485ce5e1497a840ae
Ravali-Uppu <ravaliuppu@lyrostech.com>
2024-03-11 15:24:07 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App16 from './App16';
+import App13a from './App13a';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,7 +9,7 @@ root.render(
   <React.StrictMode>
     {/* <App3 names={["John", "Cathy", "Mike"]}/> */}
    {/* <App3 students={{name:'John', age:23}}/> */}
-   <App16 />
+   <App13a />
   </React.StrictMode>
 );
 
